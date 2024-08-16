6ce0f6186e66dd7781ab2203000d814b81b90631
anismelow <anyellmendozalopez@gmail.com>
2024-08-16 14:16:32 +0200

    .

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,7 @@
+sudo apt-get update
+sudo apt-get install libmysqlclient-dev
+
+export MYSQLCLIENT_CFLAGS=$(pkg-config --cflags mysqlclient)
+export MYSQLCLIENT_LDFLAGS=$(pkg-config --libs mysqlclient)
+
 pip install -r requirements.txt
