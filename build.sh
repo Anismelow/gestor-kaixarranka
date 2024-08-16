e97a71d3009a0499c94194edd0e6e7e464015447
anismelow <anyellmendozalopez@gmail.com>
2024-08-16 15:37:13 +0200

    .

--- build.sh
+++ build.sh
@@ -10,3 +10,19 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create a superuser if needed
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+from django.core.management import call_command
+
+def create_superuser():
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='adminpassword'
+        )
+
+create_superuser()
+EOF
