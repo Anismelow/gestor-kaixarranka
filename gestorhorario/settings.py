ebc874b5a908204796eeb0eaed1f125d1433a895
anismelow <anyellmendozalopez@gmail.com>
2024-08-16 15:55:36 +0200

    .

--- gestorhorario/settings.py
+++ gestorhorario/settings.py
@@ -89,7 +89,7 @@ from decouple import config
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/gestordb_0g3n',
         conn_max_age=600
     )
 }
