a99880abc7abc96fe8eb2240c0b75c28efa8c7ce
anismelow <anyellmendozalopez@gmail.com>
2024-08-17 12:09:36 +0200

    csrf

--- gestorhorario/settings.py
+++ gestorhorario/settings.py
@@ -138,3 +138,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'  # Cambia esto a la URL de tu página de inicio de sesión
+
+
+CSRF_TRUSTED_ORIGINS = ['*']
